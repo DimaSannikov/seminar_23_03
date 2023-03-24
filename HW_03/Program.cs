cd2ca6889126d7e33a2d705b52064c4cf5747c4d
DimaSannikov <dimasannikov@gmail.com>
2023-03-24 16:45:34 +0500

    added HW_03

---
+++ HW_03/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReturnAckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+
+    if (m > 0 && n == 0)
+    {
+        return ReturnAckermannFunction(m - 1, 1);
+    }
+
+    if (m > 0 && n > 0)
+    {
+        return ReturnAckermannFunction(m - 1, ReturnAckermannFunction(m, n-1));
+    }
+    
+    return n;
+    
+}
+
+Console.WriteLine(ReturnAckermannFunction(3, 2));
