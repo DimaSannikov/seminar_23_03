611e071f26b625565e9b0be839f31daa909fed38
DimaSannikov <dimasannikov@gmail.com>
2023-03-24 11:06:02 +0500

    added EX_03

---
+++ EX_03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int SumNumber(int num)
+{
+    if (num == 0) return 0;
+    
+    return num % 10 + SumNumber(num/10);
+}
+
+Console.WriteLine(SumNumber(450));
