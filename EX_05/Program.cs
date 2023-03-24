41d1ad246941105b20e0a5d82363e5b7f8e45f7f
DimaSannikov <dimasannikov@gmail.com>
2023-03-24 11:06:13 +0500

    added EX_05

---
+++ EX_05/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+bool MultipleOfTwo(int num)
+{
+    if (num / 2 == 1) return true;
+    if (num % 2 == 1) return false;
+    return MultipleOfTwo(num / 2);
+}
+
+Console.WriteLine(MultipleOfTwo(16));
