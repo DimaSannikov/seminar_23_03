259acbd6b2dafea703d3e6ef84a303c0cba8010a
DimaSannikov <dimasannikov@gmail.com>
2023-03-24 11:06:20 +0500

    added EX_06

---
+++ EX_06/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверка на простое число: 
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+bool IsPrime(int number, int divider = 0)
+{
+    if (divider == 0) divider = number / 2;
+    if (divider == 1) return true;
+    return number % divider != 0 && IsPrime(number, divider - 1);
+}
+
+Console.WriteLine(IsPrime(11));
